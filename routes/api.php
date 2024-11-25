c01d159485c2895cc2f438be9e0889b803dcd67b
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-25 21:50:42 +0500

    I added routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::apiResource('tasks', TaskController::class);
+    Route::get('/filter/tasks', [TaskController::class, 'filterTask']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/mark-done/{id}', [TaskController::class, 'markDone']);
+});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
